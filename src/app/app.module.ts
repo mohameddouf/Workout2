6f230f0d2efe0ca77d46d506a55e77719ae137ec
aissa.amor <amor.aissa1993@gmail.com>
2022-03-07 22:52:21 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrainingComponent } from './training/training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainingComponent
   ],
   imports: [
     BrowserModule,
